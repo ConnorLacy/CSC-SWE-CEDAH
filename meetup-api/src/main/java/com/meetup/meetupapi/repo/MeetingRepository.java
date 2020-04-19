f9bf1136a03e6b6ad9610053c2de4fb9d7690453
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-19 19:11:11 -0400

    change type to long. conflict resolve

--- meetup-api/src/main/java/com/meetup/meetupapi/repo/MeetingRepository.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/repo/MeetingRepository.java
@@ -1,8 +1,10 @@
 package com.meetup.meetupapi.repo;
 
+import java.util.List;
+
 import com.meetup.meetupapi.model.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long>{
-
+    List<Meeting> findAllById(Long groupId);
 }
