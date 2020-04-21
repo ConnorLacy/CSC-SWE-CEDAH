94eb60f7909cd9b155ca993ec2465981c06f4a7c
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-21 12:52:08 -0400

    added back button

--- app/src/components/Group.js
+++ app/src/components/Group.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {formatName} from '../helper';
 import {Button, Card} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
+import group from '../assets/group.svg';
 
 const Group = (props) => {
-    console.log(props)
     return (
         <Card style={{marginBottom: 15}}>
-            <Card.Header as="h5">
-                <strong>{props.group.group_name}</strong><br/>
-                <strong>Owner</strong> {props.group.owner_fullName}
+            <Card.Header as="h5" style={{display: 'flex'}}>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <img src={group} style={{height: '3em'}}/>
+                </div>
+                <span style={{padding: '15px'}}>
+                    <strong>{props.group.group_name}</strong><br/>
+                    <strong>Owner</strong> {formatName(props.group.owner_fullName)}
+                </span>
                 </Card.Header>
             <Card.Body>
                 <Card.Text>
