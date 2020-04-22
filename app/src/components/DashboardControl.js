e890a637d29282d1761971574d70a27a4ff3ab14
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-22 06:25:29 -0400

    fixed buttons and restyled

--- app/src/components/DashboardControl.js
+++ app/src/components/DashboardControl.js
@@ -1,38 +1,33 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import {Button, Col, Container, Row} from 'react-bootstrap';
 
 const DashboardControl = (props) => {
     let type = props.tab.trim().toLowerCase()
     return (
-        <Container fluid>
-            <Col>
-                <Row style={{height: '80px',padding: '15px'}} fluid>
-                    <Col>
-                        <h1 style={{textAlign: 'left', margin: 'unset'}}>{props.tab}</h1>
-                    </Col>
-                    <Col style={{display: 'flex', alignItems: 'center'}}>
-                        {(type === 'groups') ?
-                            <>
+            <Container fluid>
+                <Col>
+                    <Row style={{height: '80px',padding: '15px'}} fluid>
+                        <Col>
+                            <h1 style={{textAlign: 'left', margin: 'unset'}}>{props.tab}</h1>
+                        </Col>
+                        <Col style={{display: 'flex', alignItems: 'center'}}>
+                            {(type === 'groups') ?
+                                <>
+                                    <Button 
+                                        variant="light">Create Group</Button>    
+                                    <Button 
+                                        onClick={props.showModal}
+                                        variant="light">Join Group</Button>
+                                </>
+                            :
                                 <Button 
-                                    as={NavLink} 
-                                    to="/group/add"
-                                    variant="light">Create Group</Button>    
-                                <Button 
-                                    as={NavLink} 
-                                    to="/group/join"
-                                    variant="light">Join Group</Button>
-                            </>
-                        :
-                            <Button 
-                                as={NavLink} 
-                                to="/group/add"
-                                variant="light">Create Meeting</Button>    
-                        }
-                    </Col>
-                </Row>
-            </Col>
-        </Container>
+                                    onClick={props.showModal}
+                                    variant="light">Create Meeting</Button>    
+                            }
+                        </Col>
+                    </Row>
+                </Col>
+            </Container>
     )
 }
 
