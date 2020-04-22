acd3ef7a14b94e0e7f3afc760eecf1e9572285b2
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-22 05:08:32 -0400

    dashboard bootstrap and customization

--- app/src/components/DashboardControl.js
+++ app/src/components/DashboardControl.js
@@ -1,50 +1,38 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import {Button} from 'react-bootstrap';
+import {Button, Col, Container, Row} from 'react-bootstrap';
 
 const DashboardControl = (props) => {
     let type = props.tab.trim().toLowerCase()
     return (
-        <>
-        <div className="control-bar">
-            <div 
-                className="filters"
-                style={{
-                    display: 'flex',
-                    alignItems: 'flex-end',
-                    width: '65%'
-                }}>
-                <h1 style={{margin: 'unset'}}>{props.tab}</h1>
-            </div>
-            <div 
-                className="controls"
-                style={{
-                    display: 'flex',
-                    width: '35%',
-                    justifyContent: 'space-between',
-                    alignItems: 'flex-end',
-                }}>
-                {(type === 'groups') ?
-                    <>
-                        <Button 
-                            as={NavLink} 
-                            to="/group/add"
-                            variant="light">Create Group</Button>    
-                        <Button 
-                            as={NavLink} 
-                            to="/group/join"
-                            variant="light">Join Group</Button>
-                    </>
-                :
-                    <Button 
-                        as={NavLink} 
-                        to="/group/add"
-                        variant="light">Create Meeting</Button>    
-                }
-            </div>
-        </div>
-        <br/>
-        </>
+        <Container fluid>
+            <Col>
+                <Row style={{height: '80px',padding: '15px'}} fluid>
+                    <Col>
+                        <h1 style={{textAlign: 'left', margin: 'unset'}}>{props.tab}</h1>
+                    </Col>
+                    <Col style={{display: 'flex', alignItems: 'center'}}>
+                        {(type === 'groups') ?
+                            <>
+                                <Button 
+                                    as={NavLink} 
+                                    to="/group/add"
+                                    variant="light">Create Group</Button>    
+                                <Button 
+                                    as={NavLink} 
+                                    to="/group/join"
+                                    variant="light">Join Group</Button>
+                            </>
+                        :
+                            <Button 
+                                as={NavLink} 
+                                to="/group/add"
+                                variant="light">Create Meeting</Button>    
+                        }
+                    </Col>
+                </Row>
+            </Col>
+        </Container>
     )
 }
 
