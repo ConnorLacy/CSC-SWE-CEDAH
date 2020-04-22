057b4ee7975b1a268da06956d513043782bdc8a9
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-22 04:33:18 -0400

    more logos

--- app/src/components/MyNav.js
+++ app/src/components/MyNav.js
@@ -2,51 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {NavLink} from 'react-router-dom';
 import {Navbar, Nav, Button} from 'react-bootstrap';
-import group from '../assets/group.svg';
+import icon_white from '../assets/icon_white.png';
 
 
 const MyNav = (props) => {
-    return (
+    if(!props.isAuthenticated) return <></>
+    else {
+        return (
         <Navbar 
             sticky="top" 
             collapseOnSelect 
             expand="lg" 
-            bg="dark" 
-            variant="dark"
-            style={{height: 75}}>
-            <Navbar.Brand as={NavLink} to="/">
-                <img alt="" src={group} style={{height: '3em'}}/>
+            style={{height: 75, background: '#1761a0'}}>
+            <Navbar.Brand>
+                <img alt="" src={icon_white} style={{height: '3em'}}/>
             </Navbar.Brand>
-            <Navbar.Brand as={NavLink} to="/">
-            {props.isAuthenticated}
+            <Navbar.Brand>
+                {props.isAuthenticated}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto"/>
                 <Nav>
-                    {props.isAuthenticated ? 
-                    <>
-                        <Button as={NavLink} 
-                                to="/dashboard"
-                                variant="success"
-                                className="mynav button"
-                                style={{margin: '0px 10px'}}>Dashboard</Button>
-                        <Button as={NavLink} to="/logout">Logout</Button>
-                    </>
-                    :
-                    <>
-                        <Button as={NavLink} 
-                                to="/login"
-                                variant="light" 
-                                className="mynav button"
-                                style={{margin: '0px 10px'}}>Login</Button>
-                        <Button as={NavLink} to="/signup">Signup</Button>
-                    </>
-                    }
+                    <Button as={NavLink} to="/logout">Logout</Button>
                 </Nav>
              </Navbar.Collapse>
         </Navbar>
-    )
+        )
+    }
 }
 
 const mapStateToProps = state => ({
