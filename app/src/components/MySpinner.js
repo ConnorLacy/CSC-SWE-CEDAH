b71f2dac30aa864a2254a44a36e985861bd8393e
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-22 18:59:48 -0400

    custom spinner from loader.io

---
+++ app/src/components/MySpinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const MySpinner = () => {
+    console.log('do something')
+    return (
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    )
+}
