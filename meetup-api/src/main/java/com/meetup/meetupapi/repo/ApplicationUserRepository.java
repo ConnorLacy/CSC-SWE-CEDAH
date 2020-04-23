e6f6167d31eac5a2c1ba6161010abc9f4351d525
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-23 05:18:40 -0400

    new jdbc query

--- meetup-api/src/main/java/com/meetup/meetupapi/repo/ApplicationUserRepository.java
+++ meetup-api/src/main/java/com/meetup/meetupapi/repo/ApplicationUserRepository.java
@@ -4,6 +4,7 @@ import com.meetup.meetupapi.model.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findById(long id);
     ApplicationUser findByUsername(String username);
     ApplicationUser findByEmail(String email);
 }
