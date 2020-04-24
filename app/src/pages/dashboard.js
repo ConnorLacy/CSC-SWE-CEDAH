0dcc55ba47030e4ee3c1c6b1707a692e90b92485
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-24 17:12:41 -0400

    fixed useEffects. Note: bug is from localstorage, not lifecyle of component

--- app/src/pages/dashboard.js
+++ app/src/pages/dashboard.js
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {formatName} from '../helper';
+import {getUserInfo} from '../redux/actions/user';
 import {getMyGroups} from '../redux/actions/groups';
 import DetailCard from '../components/cards/detail_card';
 import Group from '../components/cards/group_card';
+import Fetcher from '../pages/fetcher';
 import Calendar from '../components/calendar';
 import {Container, CardDeck, CardColumns, Tab, Row, Col, Nav} from 'react-bootstrap';
 import DashboardControl from '../components/dashboard_control';
@@ -13,15 +15,45 @@ import phone from '../assets/phone.svg';
 import mail from '../assets/mail.svg';
 
 const Dashboard = (props) => {
+    const [loading, setLoading] = useState(true)
 
-    var groups = props.groups
-    var profile = props.profile
-    var name = ''
-    var groupCards = null
-    var meetingCards = null
-    
-    if(profile) {name = formatName(props.profile.fullName).split(' ')[0]}
-    if(groups){
+    const { 
+        username, 
+        token, 
+        profile, 
+        groups, 
+        isFetching, 
+        getUserInfo,
+        getMyGroups } = props
+
+    useEffect(() => {
+        getUserInfo(username, token)
+        console.log('launched profile fetch')
+    }, [])
+
+    useEffect(() => {
+        if(profile !== null) {
+            console.log('profile here: ', profile)
+            console.log('fetching groups')
+            getMyGroups(profile.id, token)
+        }
+    }, [profile])
+
+    useEffect(() => {
+        if(groups !== null) {
+            console.log('groups here: ', groups)
+            console.log('setting loading state to false')
+            setLoading(state => isFetching)
+        }
+    }, [groups])
+
+    if(loading){
+        return (<Fetcher/>)
+    }
+    else {
+        let name = formatName(profile.fullName).split(' ')[0]
+        var groupCards = null
+        var meetingCards = null
         if (groups.length === 0) {
             groupCards = <p>Womp. You're not in any groups yet!</p>
         }
@@ -33,31 +65,19 @@ const Dashboard = (props) => {
                         members={group.members}/>
                     ))
         }
-    }
-    if(groups){
         let meetings = groups.map(({meetings})=>meetings).flat()
-        if(meetings.length>0){
-            console.log(meetings)
-            console.log('I have a meeting');
-            let cards = meetings.map((meeting, index)=> (
-                <DetailCard
-                    key={index}
-                    meeting={meeting}/>
-            ))
-            var meetingCards = (
-                <CardColumns>
-                    {cards}
-                </CardColumns>
-            )
-        }
-    }
-    
-    useEffect(() => {
-        props.getMyGroups(props.profile.id, props.token)
-    }, [props.showModal])
-
-    return (
-        <div className="page dashboard">
+        let cards = meetings.map((meeting, index)=> (
+            <DetailCard
+                key={index}
+                meeting={meeting}/>
+        ))
+        var meetingCards = (
+            <CardColumns>
+                {cards.length > 0 ? cards : <p>Nice, no meetings yet</p>}
+            </CardColumns>
+        )
+        return (
+            <div className="page dashboard">
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                 <Container fluid>
                 <Row className="myRow">
@@ -68,73 +88,77 @@ const Dashboard = (props) => {
                             paddingTop: '30px',
                             paddingBottom: '30px'
                         }}>
-                    <div className="user">
-                        <img alt="" className="large-icon" alt="" src={user}/>
-                        <h1>{name}</h1>
-                        <div className="info" >
-                            <p>
-                                <img alt="" src={phone}/>
-                                <span>{props.profile.phone}</span> 
-                            </p>
-                            <p>
-                                <img alt="" src={mail}/>
-                                <span>{props.profile.email}</span> 
-                            </p>
+                        <div className="user">
+                            <img alt="" className="large-icon" alt="" src={user}/>
+                            <h1>{name}</h1>
+                            <div className="info" >
+                                <p>
+                                    <img alt="" src={phone}/>
+                                    <span>{profile.phone}</span> 
+                                </p>
+                                <p>
+                                    <img alt="" src={mail}/>
+                                    <span>{profile.email}</span> 
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <Nav justify variant="pills" className="flex-column">
-                        <Nav.Item>
-                            <Nav.Link eventKey="first">Groups</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="second">Meetings</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="third">Calendar</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="fourth">Settings</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Col>
-                <Col sm={9} style={{height: '100%', overflow: 'auto'}}>
-                <Tab.Content>
-                    <Tab.Pane eventKey="first">
-                        <DashboardControl tab={'Groups'}/>
-                        <div style={{width: '95%', margin: 'auto'}}>
-                            {groupCards}
-                        </div>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                        <DashboardControl tab={'Meetings'}/>
-                        <CardDeck style={{width: '95%', margin: 'auto'}}>
-                            {meetingCards}
-                        </CardDeck>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                        <DashboardControl tab={'Calendar'}/>
-                        <Calendar/>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="fourth">
-                        <DashboardControl tab={'Settings'}/>
-                    </Tab.Pane>
-                </Tab.Content>
-                </Col>
-                </Row>
-                </Container>
-            </Tab.Container>
-        </div>
-    )
-}   
-    
+                        <Nav justify variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Groups</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Meetings</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Calendar</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="fourth">Settings</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col sm={9} style={{height: '100%', overflow: 'auto'}}>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            {/* <DashboardControl tab={'Groups'}/> */}
+                            <div style={{width: '95%', margin: 'auto'}}>
+                                {groupCards}
+                            </div>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                            {/* <DashboardControl tab={'Meetings'}/> */}
+                            <CardDeck style={{width: '95%', margin: 'auto'}}>
+                                {meetingCards}
+                            </CardDeck>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                            {/* <DashboardControl tab={'Calendar'}/> */}
+                            <Calendar/>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="fourth">
+                            {/* <DashboardControl tab={'Settings'}/> */}
+                        </Tab.Pane>
+                    </Tab.Content>
+                    </Col>
+                    </Row>
+                    </Container>
+                </Tab.Container>
+            </div>
+        )
+    }
+}  
+
+
 const mapStateToProps = state => ({
     token: state.user.token,
+    username: state.user.username,
     profile: state.user.profile,
     groups: state.groups.groups,
-    showModal: state.app.showModal
+    isFetching: state.app.isFetching
 })
 
 const mapDispatchToProps = dispatch => ({
+    getUserInfo: (username, token) => dispatch(getUserInfo(username, token)),
     getMyGroups : (userId, token) => dispatch(getMyGroups(userId, token))
 })
     
