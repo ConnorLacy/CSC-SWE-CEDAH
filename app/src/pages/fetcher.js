8cb513b285425a09789960908c88e2e5fc202de2
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-24 17:13:48 -0400

    cleaned up and removed responsibility

--- app/src/pages/fetcher.js
+++ app/src/pages/fetcher.js
@@ -1,39 +1,19 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import {getUserInfo} from '../redux/actions/user';
+import React from 'react';
 import {Spinner} from 'react-bootstrap';
-import {Redirect} from 'react-router-dom';
 
-const Fetcher = (props) => {
+const Fetcher = () => {
 
-    useEffect(() => {
-        props.getUserInfo(props.username, props.token)
-    }, [props.profile])
-
-    if(typeof(props.profile) !== 'undefined') return <Redirect push to="/dashboard"/>
-    else {
-        return (
-            <>
-                <div className="page logout">
-                    <div className="goodbye">
-                        <h1>Sit tight!</h1>
-                        <p>Getting your information ready.</p>
-                        <Spinner animation="border" size="lg"/>
-                    </div>
+    return (
+        <>
+            <div className="page logout">
+                <div className="goodbye">
+                    <h1>Sit tight!</h1>
+                    <p>Getting your information ready.</p>
+                    <Spinner animation="border" size="lg"/>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 }
 
-const mapStatetoProps = state => ({
-    token: state.user.token,
-    username: state.user.username,
-    profile: state.user.profile,
-})
-
-const mapDispatchToProps = dispatch => ({
-    getUserInfo: (username, token) => dispatch(getUserInfo(username, token))
-})
-
-export default connect(mapStatetoProps, mapDispatchToProps)(Fetcher);
+export default Fetcher;
