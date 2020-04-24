a87c708596be4136ff5574c33f017d9471c26f80
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-24 17:18:38 -0400

    removed unnecessary code

--- app/src/pages/logout.js
+++ app/src/pages/logout.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 import {logOut} from '../redux/actions/user';
-import {Redirect} from 'react-router-dom';
 import {MySpinner} from '../components/spinner';
 
 const Logout = (props) => {
@@ -12,29 +11,19 @@ const Logout = (props) => {
         }, 2000);
     })
     
-    if(!props.isAuthenticated) return <Redirect to="/"/>
-    else {
-        return (
-            <>
-                <div className="page logout">
-                    <div className="goodbye">
-                        <h1>Come back soon!</h1>
-                        <p>Logging you out.</p>
-                        <MySpinner/>
-                    </div>
-                </div>
-            </>
-
-        )
-    }
+    return (
+        <div className="page logout">
+            <div className="goodbye">
+                <h1>Come back soon!</h1>
+                <p>Logging you out.</p>
+                <MySpinner/>
+            </div>
+        </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
     logOut: () => dispatch(logOut())
 })
 
-const mapStateToProps = state => ({
-    isAuthenticated: state.user.isAuthenticated
-  })
-
-export default connect(mapStateToProps, mapDispatchToProps)(Logout);
+export default connect(null, mapDispatchToProps)(Logout);
