cb09631d3483fc005ea50ce16227e6a3a45ce18f
Lacy, Connor <lacyconnor1409@gmail.com>
2020-04-24 05:00:50 -0400

    added redux logger

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
+
+const loggerMiddleware = createLogger()
 
 const configureStore = (persistedState) => {
  return createStore(
    reducer,
    persistedState,
-   applyMiddleware(thunk)
+   applyMiddleware(thunk, loggerMiddleware)
  );
 }
 
