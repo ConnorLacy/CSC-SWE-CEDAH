cca092df768e574225f15ecbc8e82dd8d97a9834
ConnorLacy <lacyconnor1409@gmail.com>
2020-04-25 17:51:37 -0400

    removed group reducer

--- app/src/redux/reducers/index.js
+++ app/src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-import groupReducer from './groupReducer';
 import { appReducer } from './appReducer';
 
 export default combineReducers({
     app: appReducer,
     user: userReducer,
-    groups: groupReducer
 })
